b13f8d715f14fe66ff723d215207717a67c6dd6c
jvvaz <joao.homepersonal@gmail.com>
2019-09-11 13:49:03 -0300

    Contribuição de João Victor Vaz,turma 524

--- calculadora.py
+++ calculadora.py
@@ -1,27 +1,31 @@
-#Complete as funcoes a seguir
+# Complete as funcoes a seguir
 
-def soma(a, b):
-	#Insira o codigo aqui
+def  soma ( a , b ):
+	  print(a+b)
 
-def subtrai(a, b):
-	#Insira o codigo aqui
+def  subtrai ( a , b ):
+    print (a-b)
 
-def multiplica(a, b):
-	#Insira o codigo aqui
+def  multiplica ( a , b ):
+	print (a*b)
 
-def divide(a, b):
-	#Insira o codigo aqui
+def  divide ( a , b ):
+  #usando 'if' e 'else' para mostrar mensagem de erro, caso a variável b seja igual a "0".
+  if b == 0:
+    print('Nenhum número pode dividido por 0. Digite outro número maior que zero.')
+  else:
+    print (a/b)
 
 
-#Programa principal
+# Programa principal
 
-print("Calculadora simples")
+print ( " Calculadora simples " )
 
-num1 = float(input("Insira o primeiro numero: "))
-num2 = float(input("Insira o segundo numero: "))
+num1 =  float ( input ( " Insira o primeiro número: " ))
+num2 =  float ( input ( " Insira o segundo número: " ))
 
-soma(num1, num2)
-subtrai(num1, num2)
-multiplica(num1, num2)
-divide(num1, num2)
+soma (num1, num2)
+subtrai (num1, num2)
+multiplica (num1, num2)
+divide (num1, num2)
 
